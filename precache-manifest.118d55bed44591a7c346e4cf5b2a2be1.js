31c426cdc32f92aa2368f32902e3b16ae3b49bf9
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-24 14:45:14 +0200

    Updates

---
+++ precache-manifest.118d55bed44591a7c346e4cf5b2a2be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0904169de888faa24580ecadfac4508",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "e89c31d8d30a1857f4f5",
+    "url": "/todo-react-app/static/js/2.99794c6b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-react-app/static/js/2.99794c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3755d52144d25055f3",
+    "url": "/todo-react-app/static/js/main.496388b5.chunk.js"
+  },
+  {
+    "revision": "d9d2be8f4fec7c490ff0",
+    "url": "/todo-react-app/static/js/runtime-main.7597f4ce.js"
+  }
+]);
